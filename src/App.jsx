52f4c1c1a8babb6ffe8a05c6f85a46cf0d2e4e19
Fahridanaa <fahridanaaa@gmail.com>
2023-11-20 09:51:06 +0700

    change form to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Resume from './components/Resume/Resume';
 import CustomInput from './components/Form/CustomInput';
-import Header from './components/Form/HeaderInput';
 import './styles/App.css';
 import { useState } from 'react';
 import ResumeHeader from './components/Resume/ResumeHeader';
+import CustomForm from './components/Form/CustomForm';
 
 function App() {
 	function useFormInput(initialValue) {
@@ -15,21 +15,6 @@ function App() {
 			onChange: handleChange,
 		};
 	}
-	const [personalInfoActive, setPersonalInfoActive] = useState(false);
-	const [educationActive, setEducationActive] = useState(false);
-	const [experienceActive, setExperienceActive] = useState(false);
-
-	const handleTogglePersonalInfo = () => {
-		setPersonalInfoActive(!personalInfoActive);
-	};
-
-	const handleToggleEducation = () => {
-		setEducationActive(!educationActive);
-	};
-
-	const handleToggleExperience = () => {
-		setExperienceActive(!experienceActive);
-	};
 
 	const name = useFormInput('');
 	const address = useFormInput('');
@@ -39,67 +24,34 @@ function App() {
 	return (
 		<div id="App">
 			<div id="edit-side">
-				<div id="personal-info" className="formInput">
-					<Header
-						className="formHeader"
-						title="Personal Info"
-						id="personal-info-header"
-						handleToggle={handleTogglePersonalInfo}
-						isActive={personalInfoActive}
+				<CustomForm id="personal-info" title="Personal Info">
+					<CustomInput
+						inputType={{ type: 'text' }}
+						customLabel="Name"
+						isRecommended
+						{...name}
 					/>
-					<div
-						id="personal-info-input"
-						className={personalInfoActive ? 'show' : ''}>
-						<CustomInput
-							inputType={{ type: 'text' }}
-							customLabel="Name"
-							isRecommended
-							{...name}
-						/>
-						<CustomInput
-							inputType={{ type: 'email' }}
-							customLabel="Email"
-							isRecommended
-							{...email}
-						/>
-						<CustomInput
-							inputType={{ type: 'tel' }}
-							customLabel="Phone"
-							isRecommended
-							{...phone}
-						/>
-						<CustomInput
-							inputType={{ type: 'text' }}
-							customLabel="Address"
-							isRecommended
-							{...address}
-						/>
-					</div>
-				</div>
-				<div id="education" className="formInput">
-					<Header
-						className="formHeader"
-						title="Education"
-						id="education-header"
-						handleToggle={handleToggleEducation}
-						isActive={educationActive}
+					<CustomInput
+						inputType={{ type: 'email' }}
+						customLabel="Email"
+						isRecommended
+						{...email}
 					/>
-					<div
-						id="education-input"
-						className={educationActive ? 'show' : ''}></div>
-				</div>
-				<div id="Experience" className="formInput">
-					<Header
-						className="formHeader"
-						title="Experience"
-						id="experience-header"
-						handleToggle={handleToggleExperience}
-						isActive={experienceActive}
+					<CustomInput
+						inputType={{ type: 'tel' }}
+						customLabel="Phone"
+						isRecommended
+						{...phone}
 					/>
-					<div
-						id="experience-input"
-						className={experienceActive ? 'show' : ''}></div>
-				</div>
+					<CustomInput
+						inputType={{ type: 'text' }}
+						customLabel="Address"
+						isRecommended
+						{...address}
+					/>
+				</CustomForm>
+				<CustomForm id="education" title="Education"></CustomForm>
+				<CustomForm id="experience" title="Experience"></CustomForm>
 			</div>
 			<Resume>
 				{(name.value !== '' ||
---
+++ src/components/Form/CustomForm.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import Header from './HeaderInput';
+
+export default function CustomForm({ id, title, children }) {
+	const [isActive, setActive] = useState(false);
+
+	const handleToggle = () => {
+		setActive(!isActive);
+	};
+
+	return (
+		<div id={id} className="formInput">
+			<Header
+				className="formHeader"
+				title={title}
+				id={`${id}-header`}
+				handleToggle={handleToggle}
+				isActive={isActive}
+			/>
+			<div id={`${id}-input`} className={isActive ? 'show' : ''}>
+				{children}
+			</div>
+		</div>
+	);
+}
